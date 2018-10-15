6f3e391015e4f4c50da99f49dca2f1e04fbc2bc4
Juan Baires <juancbaires@gmail.com>
2018-10-15 15:44:06 -0400

    added some bootstrap stuf

--- bucketlist/src/App.js
+++ bucketlist/src/App.js
@@ -1,24 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Navbar } from "react-bootstrap"
+import { NavItem } from 'react-bootstrap'
+import { Nav } from 'react-bootstrap'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Navbar inverse collapseOnSelect>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <a href="#brand">React-BucketList</a>
+              </Navbar.Brand>
+              <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+              <Nav pullRight>
+                <NavItem eventKey={1} href="#">
+                  Create Item
+      </NavItem>
+                <NavItem eventKey={2} href="#">
+                  Link Right
+      </NavItem>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
         </header>
       </div>
     );
